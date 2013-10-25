de8928a649f41f448b2af001ebff93284ca856b1
arpitbbhayani <arpit.b.bhayani@gmail.com>
2013-10-26 02:17:45 +0530

    River crossing & make H2O

---
+++ geeks_non-geeks_singers.cpp
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <iomanip>
+
+#define BUCKET 0
+
+using namespace std;
+
+struct people {
+	int g;
+	int n;
+	int s;
+};
+
+/* Functions */
+void 	random_event(int);
+void * 	geek_arrives( void * );
+void * 	non_geek_arrives( void * );
+void * 	singer_arrives( void * );
+void	print_statistics( int );
+void	print_trip( int , int , int );
+int 	send_bucket();
+
+/* Global Variables */
+struct people people_count;
+struct people bucket;
+
+sem_t mutex ;
+pthread_t *thread;
+
+int k = 0;
+
+int main ( int argc , char * argv[] ) {
+
+	int l = 0;
+
+	if ( argc != 4 ) {
+		cout << "Usage : ./a.out <# geeks> <# non_geeks> <# singers>" << endl;
+		exit(0);
+	}
+
+	people_count.g = atoi ( argv[1] );
+	people_count.n = atoi ( argv[2] );
+	people_count.s = atoi ( argv[3] );
+
+	l = people_count.g + people_count.n + people_count.s;
+
+	thread = ( pthread_t * ) new pthread_t[l*2];
+
+	sem_init(&mutex,0,1);
+
+	while ( 1 ) {
+		random_event(l);
+		int s = send_bucket();
+
+		sem_wait(&mutex);
+
+		if ( s == 0 && people_count.g == 0 && people_count.n == 0 && people_count.s == 0 ) {
+			break;
+		}
+
+		sem_post(&mutex);
+	
+	}
+
+	/*
+		for ( int i = 0 ; i < l ; i++ ) {
+			cout << "i = " << i << endl;
+		pthread_join ( thread[i] , NULL );
+		}
+	*/
+
+	print_statistics( BUCKET );
+
+	return 0;
+}
+
+void reduce_count ( int g , int n , int s ) {
+
+	sem_wait(&mutex);
+
+	bucket.g -= g;
+	bucket.n -= n;
+	bucket.s -= s;
+
+	print_trip( g , n , s );
+
+	sem_post(&mutex);
+}
+
+int can_send() {
+
+	int g = bucket.g;
+	int n = bucket.n;
+	int s = bucket.s;
+
+	if ( g >= 4 && n >= 0 && s >= 0 ) {
+		reduce_count(4,0,0);
+		return 1;
+	}
+	else if ( g >= 0 && n >= 4 && s >= 0 ) {
+		reduce_count(0,4,0);
+		return 1;
+	}
+	else if ( g >= 2 && n >= 2 && s >= 0 ) {
+		reduce_count(2,2,0);
+		return 1;
+	}
+	else if ( g >= 3 && n >= 0 && s >= 1 ) {
+		reduce_count(3,0,1);
+		return 1;
+	}
+	else if ( g >= 0 && n >= 3 && s >= 1 ) {
+		reduce_count(0,3,1);
+		return 1;
+	}
+	else if ( g >= 2 && n >= 1 && s >= 1 ) {
+		reduce_count(2,1,1);
+		return 1;
+	}
+	else if ( g >= 1 && n >= 2 && s >= 1 ) {
+		reduce_count(1,2,1);
+		return 1;
+	}
+
+	return 0;
+
+}
+
+void send_the_people() {
+	sleep(1);
+}
+
+int send_bucket() {
+
+	int can_send_val = can_send();
+
+	if ( can_send_val == 1 ) {
+		send_the_people();
+	}
+	return can_send_val;
+
+}
+
+void random_event(int l) {
+
+	int n = rand() % 3;
+
+	switch ( n ) {
+
+		case 0:
+			if ( people_count.g > 0 ) {
+				pthread_create( &thread[k++] , NULL , geek_arrives , NULL );
+			}
+			break;
+		case 1:
+			if ( people_count.n > 0 ) {
+				pthread_create( &thread[k++] , NULL , non_geek_arrives , NULL );
+			}
+			break;
+		case 2:
+			if ( people_count.s > 0 ) {
+				pthread_create( &thread[k++] , NULL , singer_arrives , NULL );
+			}
+			break;
+	}
+
+}
+
+void * geek_arrives( void * arg ) {
+
+	int flag = 0;
+	sem_wait(&mutex);
+	if ( people_count.g > 0 ) {
+		people_count.g --;
+		bucket.g ++;
+	}
+	sem_post(&mutex);
+
+	return NULL;
+}
+
+void * non_geek_arrives ( void * arg ) {
+
+	int flag = 0;
+	sem_wait(&mutex);
+	if ( people_count.n > 0 ) {
+		people_count.n --;
+		bucket.n ++;
+	}
+	sem_post(&mutex);
+	return NULL;
+}
+
+void * singer_arrives ( void * arg ) {
+
+	int flag = 0;
+	sem_wait(&mutex);
+	if ( people_count.s > 0 ) {
+		people_count.s --;
+		bucket.s ++;
+	}
+	sem_post(&mutex);
+	return NULL;
+}
+
+/*
+void print_header() {
+	cout << endl << setw(46) << "Statistics" << endl;
+	cout << endl << setw(46) << "Trips" << endl;
+	cout << "+-----------------------------------------------------------------------------------------------------------+" << endl;
+	cout << "|           # Trip         |          # Geek         |        # Non-Geek        |          # Singer         |" << endl;
+	cout << "+-----------------------------------------------------------------------------------------------------------+" << endl;
+}
+*/
+
+void print_trip ( int g , int n , int s ) {
+	cout << "# Geeks : "  << g << "      # Non Geek : " << n << "      # Singers : " << s << endl;
+}
+
+void print_statistics( int type ) {
+	sleep(1);
+
+	cout << endl << setw(46) << "People left" << endl;
+	cout << "+--------------------------------------------------------------------------------+" << endl;
+	cout << "|          # Geek         |        # Non-Geek        |          # Singer         |" << endl;
+	cout << "+--------------------------------------------------------------------------------+" << endl;
+		cout << "|" << setw(14) << bucket.g << setw(12)  << "|" << setw(14) << bucket.n << setw(13) << "|" << setw(15) << bucket.s << setw(13) << "|" << endl;
+	cout << "+--------------------------------------------------------------------------------+" << endl;
+
+}
---
+++ makeH2O.cpp
@@ -0,0 +1,186 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+#include <iomanip>
+
+using namespace std;
+
+struct value {
+	int h;
+	int o;
+	int s;
+	int e;
+};
+
+struct site_data {
+	int e;
+};
+
+/* Global Variables */
+
+sem_t	    allow_site , energy_threshold , reaction;
+sem_t 	  * site_sem;
+pthread_t * site_threads;
+site_data * s_d;
+struct value v;
+
+/* Functions */
+
+void * site_thread ( void * );
+void print_statistics();
+void setup_environment();
+
+
+void setup_environment() {
+
+	site_sem	= ( sem_t * ) 	  malloc ( v.s * sizeof(sem_t) );
+	site_threads    = ( pthread_t * ) malloc ( v.s * sizeof(pthread_t) );
+	s_d	    	= ( site_data * ) malloc ( v.s * sizeof(site_data) );
+
+	sem_init ( &allow_site , 0 , 0 );
+	sem_init ( &energy_threshold , 0 , v.e );
+	sem_init ( &reaction , 0 , 1 );
+
+	/* Initially at all sites reaction can happen and energy at each site = 0 */
+	for ( int i = 0 ; i < v.s ; i++ ) {
+		sem_init ( &site_sem[i] , 0 , 1 );
+		s_d[i].e = 0;
+		
+	}
+
+	/* Create thread for each site */
+	for ( int i = 0 ; i < v.s ; i++ ) {
+		pthread_create ( &site_threads[i] , NULL , site_thread , NULL );
+	}
+
+}
+
+void * site_thread ( void * arg ) {
+
+	while ( 1 ) {
+
+		sem_wait ( &allow_site );
+
+		int site_selected = rand() % v.s;
+
+		if ( v.s == 1 ) {
+
+		}
+		else if ( site_selected == 0 ) {
+			/* First site selected */
+			sem_wait ( &site_sem[1] );
+			sem_post ( &site_sem[1] );
+		}
+		else if ( site_selected == v.s - 1 ) {
+			/* Last site selected */
+			sem_wait ( &site_sem[v.s - 2] );
+			sem_post ( &site_sem[v.s - 2] );
+		}
+		else {
+			/* Any of the non-extreme site selected */
+			sem_wait ( &site_sem[site_selected - 1] );
+			sem_post ( &site_sem[site_selected - 1] );
+			sem_wait ( &site_sem[site_selected + 1] );
+			sem_post ( &site_sem[site_selected + 1] );
+		}
+
+		/* Locking current site */
+		sem_wait ( &site_sem[site_selected] );
+
+		/* Allowing other site to be selected */
+		sem_post ( &allow_site );
+
+		/* Reaction Start - { Critical Region } */
+		sem_wait ( &reaction );
+
+		/* If hydrogen and oxygen atoms are inadequate for any further reaction then terminate the code */
+		if ( v.h < 2 || v.o < 1 ) {
+			print_statistics();
+			exit (0);
+		}
+
+		sem_wait ( &energy_threshold );
+
+		v.h -= 2;
+		v.o -= 1;
+
+		s_d[site_selected].e += 1;
+
+		/*
+			int simul_reaction = 0;
+			sem_getvalue( &energy_threshold , &simul_reaction );
+			simul_reaction = v.e - simul_reaction;
+		*/
+
+		cout << "Reaction at site # : " << setw(3) << site_selected + 1 ;
+		cout << "    # H-atoms : " << setw(3) << v.h ;
+		cout << "    # O-atoms : " << setw(3) << v.o ;
+		cout << "    # energy released : " << setw(3) << s_d[site_selected].e << " EMj " << endl;
+
+		sem_post ( &reaction );
+
+		/* Time for reaction @ current site */
+		sleep(1);
+
+		sem_post ( &energy_threshold );
+		sem_post ( &site_sem[site_selected] );
+
+		/* {End of Critical Region} */
+
+	}
+
+	return NULL;
+}
+
+int main ( int argc , char * argv[] ) {
+
+	if ( argc != 5 ) {
+		cout << "Usage : ./a.out <# h atoms> <# o atoms> <# sites> <energy>" << endl;
+		exit(0);
+	}
+
+	v.h = atoi ( argv[1] );
+	v.o = atoi ( argv[2] );
+	v.s = atoi ( argv[3] );
+	v.e = atoi ( argv[4] );
+
+	if ( v.e == 0 ) {
+		cout << "Energy threshold cannot be zero." << endl;
+		exit(0);
+	}
+
+	setup_environment();
+
+	/*
+		Allow any one site to enter the critical region of REACTION.
+		This will start the process of generating water.
+	*/
+	sem_post ( &allow_site );
+
+	for ( int i = 0 ; i < v.s ; i++ ) {
+		pthread_join ( site_threads[i] , NULL );
+	}
+
+	return 0;
+}
+
+void print_statistics() {
+	sleep(1);
+	cout << endl << setw(32) << "Statistics\n" << endl;
+	cout << setw(33) << "Site information" << endl;
+	cout << "+---------------------------------------------------------+" << endl;
+	cout << "|" << setw(25) << "Site #         |" << "          Energy Released       |" << endl;
+	cout << "+---------------------------------------------------------+" << endl;
+	for ( int i = 0 ; i < v.s ; i++ ) {
+		cout << "|" << setw(13) << i+1 << setw(12)  << "|" << setw(14) << s_d[i].e << " EMJ" << setw(15) << "|" << endl;
+	}
+	cout << "+---------------------------------------------------------+" << endl << endl;
+	cout << endl << setw(36) << "H-atoms & O-atoms Left" << endl;
+	cout << "+---------------------------------------------------------+" << endl;
+	cout << "|" << setw(25) << "# H-atoms        |" << "           # O-atoms            |" << endl;
+	cout << "+---------------------------------------------------------+" << endl;
+	cout << "|" << setw(13) << v.h << setw(12)  << "|" << setw(16) << v.o << "" << setw(17) << "|" << endl;
+	cout << "+---------------------------------------------------------+" << endl << endl;
+}
